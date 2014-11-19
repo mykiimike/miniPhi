24f51b61f20fbc0d785a56830c439b16b7e738a6
mykiimike <mv@verman.fr>
2014-11-19 19:19:10 +0100

    SPI implementation nearly done

--- include/msp430/spi.h
+++ include/msp430/spi.h
@@ -23,6 +23,8 @@
 
 	typedef struct mp_spi_s mp_spi_t;
 
+	typedef void (*mp_spi_callback_t)(mp_spi_t *);
+
 	typedef enum {
 		MP_SPI_CLK_PHASE_CHANGE,
 		MP_SPI_CLK_PHASE_CAPTURE
@@ -39,6 +41,11 @@
 	} mp_spi_role_t;
 
 	typedef enum {
+		MP_SPI_MSB,
+		MP_SPI_LSB
+	} mp_spi_first_t;
+
+	typedef enum {
 		MP_SPI_MODE_3PIN,
 		MP_SPI_MODE_4PIN_LOW,
 		MP_SPI_MODE_4PIN_HIGH
@@ -48,6 +55,11 @@
 		MP_SPI_BIT_7,
 		MP_SPI_BIT_8,
 	} mp_spi_bit_t;
+
+	typedef enum {
+		MP_SPI_SYNC,
+		MP_SPI_ASYNC,
+	} mp_spi_sync_t;
 
 	struct mp_spi_s {
 		/** some architectures require a gate */
@@ -71,11 +83,64 @@
 		/** who is handling SPI */
 		char *who;
 
+#define MP_SPI_TX_BUFFER 400
+#define MP_SPI_RX_BUFFER 80
+
+		unsigned char tx_buffer[MP_SPI_TX_BUFFER];
+		unsigned int tx_size;
+		unsigned int tx_pos;
+
+		unsigned char rx_buffer[MP_SPI_RX_BUFFER];
+		unsigned int rx_size;
+
+		mp_spi_callback_t onRead;
+		mp_spi_callback_t onWriteEnd;
+
+		/** internal: gate */
+		mp_gate_t *_gate;
 		mp_gpio_port_t *_simo;
 		mp_gpio_port_t *_somi;
 		mp_gpio_port_t *_clock;
 		mp_gpio_port_t *_ste;
 		mp_list_item_t item;
+
 	};
+
+	#define _SPI_CTLW0   0x00
+	#define _SPI_CTL0    0x01
+	#define _SPI_CTL1    0x00
+	#define _SPI_BRW     0x06
+	#define _SPI_BR0     0x06
+	#define _SPI_BR1     0x07
+	#define _SPI_MCTL    0x08
+	#define _SPI_STATW   0x0a
+	#define _SPI_RXBUF   0x0c
+	#define _SPI_TXBUF   0x0e
+
+	#define _SPI_IE      0x1c
+	#define _SPI_IFG     0x1d
+	#define _SPI_IV      0x1e
+
+	#define _SPI_REG8(_port, _type) \
+		*((volatile char *)(_port->_baseAddress+_type))
+
+
+	static inline void mp_spi_enable_rx(mp_spi_t *spi) {
+		/* check CTS */
+		_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCRXIE;
+	}
+
+	static inline void mp_spi_disable_rx(mp_spi_t *spi) {
+		_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~UCRXIE;
+	}
+
+	static inline void mp_spi_enable_tx(mp_spi_t *spi) {
+		/* check CTS */
+		_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCTXIE;
+	}
+
+	static inline void mp_spi_disable_tx(mp_spi_t *spi) {
+		_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~UCTXIE;
+	}
 
 #endif
--- msp430/spi.c
+++ msp430/spi.c
@@ -20,10 +20,13 @@
 
 #include <mp.h>
 
-
+/* internal pointers */
 static mp_list_t __spi;
 static unsigned int __spi_count;
 
+static unsigned char __mp_spi_rx(mp_spi_t *spi);
+static void __mp_spi_tx(mp_spi_t *spi, unsigned char data);
+static void __mp_spi_interrupt(void *user);
 
 void mp_spi_init() {
 	mp_list_init(&__spi);
@@ -34,23 +37,260 @@ void mp_spi_fini() {
 /* should be ok */
 }
 
-
 mp_ret_t mp_spi_open(
 		mp_spi_t *spi, char *who,
 		mp_spi_phase_t phase,
 		mp_spi_polarity_t polarity,
+		mp_spi_first_t first,
 		mp_spi_role_t role,
 		mp_spi_mode_t mode,
-		mp_spi_bit_t bit
+		mp_spi_bit_t bit,
+		mp_spi_sync_t sync,
+		unsigned long frequency
 	) {
+	unsigned int prescaler;
+
+	spi->frequency = frequency;
+
+	/* get gate Id*/
+	spi->_gate = mp_gate_handle(spi->gateId, "SPI");
+	if(spi->_gate == NULL)
+		return(FALSE);
+
+	/* open gpio ports */
+	spi->_simo = mp_gpio_handle(spi->simo.port, spi->simo.pin, "SPI SIMO");
+	if(spi->_simo == NULL) {
+		mp_gate_release(spi->_gate);
+		return(FALSE);
+	}
+
+	spi->_somi = mp_gpio_handle(spi->somi.port, spi->somi.pin, "SPI SOMI");
+	if(spi->_somi == NULL) {
+		mp_gpio_release(spi->_simo);
+		mp_gate_release(spi->_gate);
+		return(FALSE);
+	}
+
+	spi->_clock = mp_gpio_handle(spi->clock.port, spi->clock.pin, "SPI CLOCK");
+	if(spi->_clock == NULL) {
+		mp_gpio_release(spi->_simo);
+		mp_gpio_release(spi->_somi);
+		mp_gate_release(spi->_gate);
+		return(FALSE);
+	}
+
+	if(spi->ste.port > 0) {
+		spi->_ste = mp_gpio_handle(spi->ste.port, spi->ste.pin, "SPI STE");
+		if(spi->_ste == NULL) {
+			mp_gpio_release(spi->_clock);
+			mp_gpio_release(spi->_somi);
+			mp_gpio_release(spi->_simo);
+			mp_gate_release(spi->_gate);
+			return(FALSE);
+		}
+	}
+
+	/* set port alternate function and port direction */
+	_GPIO_REG8(spi->_simo, _GPIO_SEL) |= 1<<spi->_simo->pin;
+	_GPIO_REG8(spi->_somi, _GPIO_SEL) |= 1<<spi->_somi->pin;
+	_GPIO_REG8(spi->_clock, _GPIO_SEL) |= 1<<spi->_clock->pin;
+	if(spi->_ste) {
+		_GPIO_REG8(spi->_ste, _GPIO_SEL) |= 1<<spi->_ste->pin;
+		/* set output ste */
+		_GPIO_REG8(spi->_ste, _GPIO_DIR) |= 1<<spi->_ste->pin;
+	}
+
+	_GPIO_REG8(spi->_simo, _GPIO_DIR) |= 1<<spi->_simo->pin;
+	_GPIO_REG8(spi->_somi, _GPIO_DIR) &= ~(1<<spi->_somi->pin);
+	_GPIO_REG8(spi->_clock, _GPIO_DIR) |= 1<<spi->_clock->pin;
+
+	/* open for write */
+	_SPI_REG8(spi->_gate, _SPI_CTL1) |= UCSWRST;
+
+	/* clock source - SMCLK */
+	_SPI_REG8(spi->_gate, _SPI_CTL1) |= 0x80;
+	UCB2CTL1 = UCB2CTL1 | 0x80;                     //
+
+	prescaler = mp_clock_get_speed() / frequency;
+	_SPI_REG8(spi->_gate, _SPI_BR0) = prescaler % 256;
+	_SPI_REG8(spi->_gate, _SPI_BR1) = prescaler / 256;
+
+	/* user options */
+	switch(phase) {
+		case MP_SPI_CLK_PHASE_CHANGE:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UCCKPH);
+			break;
+		case MP_SPI_CLK_PHASE_CAPTURE:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCCKPH;
+			break;
+	}
+
+	switch(polarity) {
+		case MP_SPI_CLK_POLARITY_LOW:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UCCKPL);
+			break;
+		case MP_SPI_CLK_POLARITY_HIGH:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCCKPL;
+			break;
+	}
+
+	switch(first) {
+		case MP_SPI_LSB:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UCMSB);
+			break;
+		case MP_SPI_MSB:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCMSB;
+			break;
+	}
+
+	switch(role) {
+		case MP_SPI_SLAVE:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UCMST);
+			break;
+		case MP_SPI_MASTER:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCMST;
+			break;
+	}
+
+	switch(mode) {
+		case MP_SPI_MODE_3PIN:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCMODE_0;
+			break;
+		case MP_SPI_MODE_4PIN_LOW:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCMODE_1;
+			break;
+		case MP_SPI_MODE_4PIN_HIGH:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCMODE_2;
+			break;
+	}
+
+	switch(bit) {
+		case MP_SPI_BIT_8:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UC7BIT);
+			break;
+		case MP_SPI_BIT_7:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UC7BIT;
+			break;
+	}
+
+	switch(sync) {
+		case MP_SPI_ASYNC:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) &= ~(UCSYNC);
+			break;
+		case MP_SPI_SYNC:
+			_SPI_REG8(spi->_gate, _SPI_CTL0) |= UCSYNC;
+			break;
+	}
+
+	/* write finished */
+	_SPI_REG8(spi->_gate, _SPI_CTL1) &= ~(UCSWRST);
+
+	/* place interrupt */
+	mp_interrupt_set(spi->_gate->_ISRVector, __mp_spi_interrupt, spi, spi->_gate->portDevice);
+
+	/* list */
 	mp_list_add_last(&__spi, &spi->item, spi);
 	__spi_count++;
 	return(TRUE);
 }
 
 mp_ret_t mp_spi_close(mp_spi_t *spi) {
+
+	/* disble interrupts */
+	mp_spi_disable_rx(spi);
+	mp_spi_disable_tx(spi);
+	mp_interrupt_unset(spi->_gate->_ISRVector);
+
+	_GPIO_REG8(spi->_simo, _GPIO_SEL) &= ~(1<<spi->_simo->pin);
+	_GPIO_REG8(spi->_somi, _GPIO_SEL) &= ~(1<<spi->_somi->pin);
+	_GPIO_REG8(spi->_clock, _GPIO_SEL) &= ~(1<<spi->_clock->pin);
+
+	if(spi->_ste) {
+		_GPIO_REG8(spi->_ste, _GPIO_SEL) &= ~(1<<spi->_ste->pin);
+		/* set output ste */
+		_GPIO_REG8(spi->_ste, _GPIO_DIR) &= ~(1<<spi->_ste->pin);
+	}
+
 	mp_list_remove(&__spi, &spi->item);
 	__spi_count--;
 	return(TRUE);
+}
+
+void mp_spi_write(mp_spi_t *spi, unsigned char *input) {
+	int size = 2;
+	int rest;
+	int a;
+
+	/* disable TX interrupt */
+	mp_spi_disable_tx(spi);
+
+	rest = sizeof(spi->tx_buffer)-spi->tx_size;
+
+	/* no more space flush output now */
+	if(rest < size) {
+
+		/* flush now */
+		for(a=spi->tx_pos; a<spi->tx_size; a++)
+			__mp_spi_tx(spi, spi->tx_buffer[a]);
+		spi->tx_size = 0;
+		spi->tx_pos = 0;
+
+		/* always place */
+		memcpy(spi->tx_buffer, input, size);
+		spi->tx_size += size;
+	}
+	/* space available */
+	else {
+		memcpy(spi->tx_buffer+spi->tx_pos, input, size);
+		spi->tx_size += size;
+	}
+
+	/* enable TX interrupt */
+	mp_spi_enable_tx(spi);
+}
+
+static unsigned char __mp_spi_rx(mp_spi_t *spi) {
+	if (_SPI_REG8(spi->_gate, _SPI_IFG) & UCRXIFG)
+		return(_SPI_REG8(spi->_gate, _SPI_RXBUF));
+	return 0;
+}
+
+static void __mp_spi_tx(mp_spi_t *spi, unsigned char data) {
+  while (!(_SPI_REG8(spi->_gate, _SPI_IFG) & UCRXIFG));
+  _SPI_REG8(spi->_gate, _SPI_TXBUF) = data;
+}
+
+static void __mp_spi_interrupt(void *user) {
+	mp_spi_t *spi = user;
+
+	switch(_SPI_REG8(spi->_gate, _SPI_IFG)) {
+		case UCRXIFG:
+			spi->rx_buffer[spi->rx_size] = __mp_spi_rx(spi);
+			spi->rx_size++;
+
+			/* manage callbacks */
+			if(spi->rx_size%2 == 0)
+				spi->onWriteEnd(spi); /* use task wakeup */
+
+			break;
+
+		case UCTXIFG:
+			/* no more data to send */
+			if(spi->tx_size == 0 || spi->tx_pos == spi->tx_size) {
+				mp_spi_disable_tx(spi);
+				if(spi->onWriteEnd != NULL && spi->tx_size > 0 || spi->tx_pos == spi->tx_size)
+					spi->onWriteEnd(spi); /* use task wakeup */
+				spi->tx_pos = 0;
+				spi->tx_size = 0;
+				return;
+			}
+
+			/* send last char */
+			__mp_spi_tx(spi, spi->tx_buffer[spi->tx_pos]);
+			spi->tx_pos++;
+
+			break;
+
+	}
 
 }
