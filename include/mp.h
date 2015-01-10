747ed89837307c597ed0028c3e8e6d58bf074e46
mykiimike <mv@verman.fr>
2015-01-10 13:12:42 +0100

    Updating branch to 1.0.3

--- include/mp.h
+++ include/mp.h
@@ -98,7 +98,7 @@
 	#include "drivers/sensors/LSM9DS0.h"
 	#include "drivers/sensors/TMP006.h"
 
-	#define MP_KERNEL_VERSION "1.0.2"
+	#define MP_KERNEL_VERSION "1.0.3"
 
 	struct mp_kernel_s {
 		/** Vendor name  */
